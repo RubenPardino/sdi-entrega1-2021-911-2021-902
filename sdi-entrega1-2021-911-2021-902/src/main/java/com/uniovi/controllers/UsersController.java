32aacb2d2dcf5d3bcd390a0c8d74c9893064a234
uo271397 <uo271397@uniovi.es>
2021-03-08 16:50:43 +0100

    Preparación del esqueleto para trabajar

--- sdi-entrega1-2021-911-2021-902/src/main/java/com/uniovi/controllers/UsersController.java
+++ sdi-entrega1-2021-911-2021-902/src/main/java/com/uniovi/controllers/UsersController.java
@@ -1,5 +1,23 @@
 package com.uniovi.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.uniovi.entities.User;
+
+@Controller
 public class UsersController {
 
+	@RequestMapping(value = "/signup", method = RequestMethod.GET)
+	public String signup(Model model) {
+		model.addAttribute("user", new User());
+		return "signup";
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(Model model) {
+		return "login";
+	}
 }
