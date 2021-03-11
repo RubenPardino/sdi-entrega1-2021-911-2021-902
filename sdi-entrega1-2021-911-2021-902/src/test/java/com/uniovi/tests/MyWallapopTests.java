27bdc9b9b693615e29238f5c354b534c6bfa91cd
UO266640 <jony23699@gmail.com>
2021-03-11 19:14:24 +0100

    Tests cerrar sesion

--- sdi-entrega1-2021-911-2021-902/src/test/java/com/uniovi/tests/MyWallapopTests.java
+++ sdi-entrega1-2021-911-2021-902/src/test/java/com/uniovi/tests/MyWallapopTests.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import com.uniovi.tests.pageobjects.PO_HomeView;
-
+import com.uniovi.tests.pageobjects.PO_LoginView;
 import com.uniovi.tests.pageobjects.PO_Properties;
 import com.uniovi.tests.pageobjects.PO_RegisterView;
 import com.uniovi.tests.pageobjects.PO_View;
@@ -22,7 +22,7 @@ import org.junit.runners.MethodSorters;
 //Ordenamos las pruebas por el nombre del métodos
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class RegisterTests {
+public class MyWallapopTests {
 
 	// En Windows (Debe ser la versión 65.0.1 y desactivar las actualizacioens
 	// automáticas)):
@@ -74,9 +74,7 @@ public class RegisterTests {
 		PO_RegisterView.fillForm(driver, "test@gmail.com", "Jonathan", "Barbon", "123456", "123456");
 		// Comprobamos que entramos en la sección privada
 		PO_View.checkElement(driver, "text", "Bienvenidos a la pagina principal");
-		// Vamos al formulario de registro
 		PO_HomeView.clickOption(driver, "logout", "class", "btn btn-primary");
-		// Comprobamos que entramos en la sección privada
 		PO_View.checkElement(driver, "text", "Identificate");
 	}
 
@@ -113,6 +111,69 @@ public class RegisterTests {
 		// Rellenamos el formulario.
 		PO_RegisterView.fillForm(driver, "test@gmail.com", "Jonathan", "Barbon", "123456", "123456");
 		PO_RegisterView.checkKey(driver, "Error.signup.email.duplicate", PO_Properties.getSPANISH());
+		PO_HomeView.clickOption(driver, "logout", "class", "btn btn-primary");
+		// Comprobamos que entramos en la sección privada
+		PO_View.checkElement(driver, "text", "Identificate");
+	}
+
+	// Hacer click en la opción de salir de sesión y comprobar que se redirige a la
+	// página de inicio de sesión (Login)
+	@Test
+	public void PR10() {
+		PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+		// Rellenamos el formulario.
+		PO_LoginView.fillForm(driver, "a@gmail.com", "123456");
+		// Comprobamos que entramos en la sección privada
+		PO_View.checkElement(driver, "text", "Bienvenidos a la pagina principal");
+		PO_HomeView.clickOption(driver, "logout", "class", "btn btn-primary");
+		PO_View.checkElement(driver, "text", "Identificate");
+
+	}
+
+	// Comprobar que el botón cerrar sesión no está visible si el usuario no está
+	// autenticado.
+	@Test
+	public void PR11() {
+		// Vamos al formulario de registro
+		PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+		PO_View.checkElement(driver, "text", "Identificate");
+
+	}
+
+	// Registro de Usuario con datos válidos
+	@Test
+	public void PR28() {
+		// Intentar acceder sin estar autenticado a la opción de listado de usuarios del
+		// administrador. Se deberá volver al formulario de login
+
+		driver.navigate().to("http://localhost:8090/users/list");
+		PO_View.checkElement(driver, "text", "Identificate");
+
+	}
+
+	// Intentar acceder sin estar autenticado a la opción de listado de ofertas
+	// propias de un usuario estándar. Se deberá volver al formulario de login
+	@Test
+	public void PR29() {
+		// Vamos al formulario de registro
+		driver.navigate().to("http://localhost:8090/product/myList");
+		PO_View.checkElement(driver, "text", "Identificate");
+
+	}
+
+	// Estando autenticado como usuario estándar intentar acceder a la opción de
+	// listado de usuarios del administrador. Se deberá indicar un mensaje de acción
+	// prohibida
+	@Test
+	public void PR30() {
+		PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+		// Rellenamos el formulario.
+		PO_LoginView.fillForm(driver, "a@gmail.com", "123456");
+		// Comprobamos que entramos en la sección privada
+		PO_View.checkElement(driver, "text", "Bienvenidos a la pagina principal");
+		driver.navigate().to("http://localhost:8090/user/list");
+
+		PO_View.checkElement(driver, "free", "//h1[contains(text(),'HTTP Status 403 – Forbidden')]");
 
 	}
 }
