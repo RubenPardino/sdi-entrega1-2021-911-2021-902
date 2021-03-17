c672b57cf42e7b1266ec79ae842793ba9936df98
UO266640 <jony23699@gmail.com>
2021-03-17 18:45:03 +0100

    Listado de ofertas compradas

--- sdi-entrega1-2021-911-2021-902/src/main/java/com/uniovi/controllers/ProductsController.java
+++ sdi-entrega1-2021-911-2021-902/src/main/java/com/uniovi/controllers/ProductsController.java
@@ -58,6 +58,25 @@ public class ProductsController {
 		model.addAttribute("page", Products);
 		return "product/list";
 	}
+	
+	
+	@RequestMapping("/product/list/compradas") // "/product/myList"
+	public String getListCompradas(Model model, Pageable pageable, Principal principal,
+			@RequestParam(value = "", required = false) String searchText) {
+		String email = principal.getName();
+		User user = usersService.getUserByEmail(email);
+		Page<Product> Products = new PageImpl<Product>(new LinkedList<Product>());
+
+		if (searchText != null && !searchText.isEmpty()) {
+			Products = ProductsService.searchProductsByTitle(pageable, searchText, user);
+		} else {
+			Products = ProductsService.getProducts(pageable);
+		}
+		model.addAttribute("user", user);
+		model.addAttribute("productList", Products.getContent());
+		model.addAttribute("page", Products);
+		return "product/listCompradas";
+	}
 
 	@RequestMapping("/product/myList")
 	public String getMyList(Model model, Pageable pageable, Principal principal,
