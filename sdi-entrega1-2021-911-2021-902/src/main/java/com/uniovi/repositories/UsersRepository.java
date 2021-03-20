0e2e63ee5029fe5ece350dfdda77cbc6f4524b7c
uo271397 <uo271397@uniovi.es>
2021-03-20 14:45:58 +0100

    Solucionado botón de buscar usuarios

--- sdi-entrega1-2021-911-2021-902/src/main/java/com/uniovi/repositories/UsersRepository.java
+++ sdi-entrega1-2021-911-2021-902/src/main/java/com/uniovi/repositories/UsersRepository.java
@@ -10,7 +10,7 @@ import org.springframework.data.repository.CrudRepository;
 public interface UsersRepository extends CrudRepository<User, Long> {
 	User findByEmail(String email);
 
-	@Query("SELECT r FROM User r WHERE (LOWER (r.name) LIKE LOWER(?1) )")
+	@Query("SELECT r FROM User r WHERE (LOWER (r.name) LIKE LOWER(?1) OR LOWER (r.lastName) LIKE LOWER(?1))")
 	Page<User> searchUsersByNameOrSurname(Pageable pageable, String searchText);
 
 	Page<User> findAll(Pageable pageable);
